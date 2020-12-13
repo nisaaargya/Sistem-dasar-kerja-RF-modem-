23433139ebfc47a3e0ff326c541acd3f4d9ca30b
nisaaargya <73020824+nisaaargya@users.noreply.github.com>
2020-12-13 17:11:54 +0700

    Add files via upload

---
+++ Bagian_Transmitter.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+int TEMP_SENSOR=A0;
+
+void setup() {
+  Serial.begin(9600);
+  lcd.setCursor(0,0);
+  lcd.print("wireless Serial ");
+  lcd.setCursor(0,1);
+  lcd.print("communication via 2.4 GHz");
+  lcd.setCursor(0,2);
+  lcd.print("modem at PNJ.....");
+}
+
+void loop() {
+  int TEMP_SENSOR_ADC_VALUE = analogRead(TEMP_SENSOR);
+  int TEMP_VAL=TEMP_SENSOR_ADC_VALUE/2; 
+  lcd.setCursor(0,3);
+  lcd.print("Temp. in 0C: ");
+  lcd.print(TEMP_VAL);
+  Serial.write(TEMP_VAL);
+  delay(100);
+}
